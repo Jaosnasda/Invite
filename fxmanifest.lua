0ef9425c6ee10a3329b3f88a7a2704234166ffce
Jaosnasda <126997457+Jaosnasda@users.noreply.github.com>
2023-06-06 18:45:55 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,8 @@ version '1.0.0'
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Jaosnasda#2222'
-description 'Job Invite | Made by Jaosnasda#2222'
+author 'Ogrmasa#1799'
+description 'Job Invite | Made by Ogrmasa#1799'
 
 server_scripts = {
     'config.lua',
@@ -16,14 +16,14 @@ shared_script '@es_extended/imports.lua'
 
 --[[ 
 
-    ==== Job Invite - Jaosnasda#2222 ====
+    ==== Job Invite - Ogrmasa#1799 ====
 
     Invite players to your job with a simple command!
     Players can accept the invite with a simple command!
     Fast and easy to use! Simply add the jobs you want to the config and you are ready to go!
     
-    Coder: Jaosnasda#2222
+    Coder: Ogrmasa#1799
 
-    ==== Job Invite - Jaosnasda#2222 ==== 
+    ==== Job Invite - Ogrmasa#1799 ====
 
 ]]
